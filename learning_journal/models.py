43ea50fc4f511fd77c786d6c9643a5b626fc4d18
Clint Garwood <clinton@unknown28373724622a.attlocal.net>
2015-04-09 18:41:53 -0500

    add sql tablename Entry

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -24,4 +24,12 @@ class MyModel(Base):
     name = Column(Text)
     value = Column(Integer)
 
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text(255), unique=True)
+    body = Column(Unicode)
+	created = Column(DateTime(now))
+	edited = Column(DateTime(now))
+
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
