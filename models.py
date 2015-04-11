7759dfe844f5755fa5ca3f9226d37ae809ef805c
garwoodpr <garwoodpr@users.noreply.github.com>
2015-04-11 12:13:42 -0500

    Update models.py
    why isn't this updating from my local repo?

--- models.py
+++ models.py
@@ -79,21 +79,21 @@ class Entry(Base):
 	created = Column(DateTime, default=datetime.datetime.utcnow)
 	edited = Column(DateTime, default=datetime.datetime.utcnow)
 
-@classmethod
-def all(cls, session=None):
-	"""return a query with all entries,
-	ordered by creation date reversed"""
-	if session is None:
-		session = DBSession
-		return session.query(cls).order_by(sa.desc(cls.created)).all()
-
-@classmethod
-def by_id(cls, id, session=None):
-	"""return a single entry identified by id
-	If no entry exists with the provided id, return None
-	"""
-	if session is None:
-		session = DBSession
-		return session.query(cls).get(id)
+	@classmethod
+	def all(cls, session=None):
+		"""return a query with all entries,
+		ordered by creation date reversed"""
+		if session is None:
+			session = DBSession
+			return session.query(cls).order_by(sa.desc(cls.created)).all()
+	
+	@classmethod
+	def by_id(cls, id, session=None):
+		"""return a single entry identified by id
+		If no entry exists with the provided id, return None
+		"""
+		if session is None:
+			session = DBSession
+			return session.query(cls).get(id)
 	
 	
